e68842b1b0e9b6beea910a9759f538602a11de48
GILDARTS0 <77912946+GILDARTS0@users.noreply.github.com>
2022-03-20 14:05:49 +0530

    Update and rename main.py to text_extraction.py

--- text_extraction.py
+++ text_extraction.py
@@ -8,15 +8,15 @@ print(pytesseract.image_to_string(img))
 
 ## Detecting Characters
 
-hImg,wImg,_ = img.shape
+H_Img,W_Img,_ = img.shape
 pytesseract.image_to_boxes(img)
 boxes = pytesseract.image_to_boxes(img)
 
 for b in boxes.splitlines():
     b = b.split(' ')
     x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
-    cv2.rectangle(img, (x,hImg- y), (w,hImg- h), (0, 128, 0), 1)
-    cv2.putText(img,b[0],(x,hImg- y+20),cv2.FONT_ITALIC,0.6,(50,50,255),1)
+    cv2.rectangle(img, (x,H_Img- y), (w,H_Img- h), (0, 128, 0), 1)
+    cv2.putText(img,b[0],(x,H_Img- y+20),cv2.FONT_ITALIC,0.6,(50,50,255),1)
 
 cv2.imshow('Result',img)
 cv2.waitKey(0)
